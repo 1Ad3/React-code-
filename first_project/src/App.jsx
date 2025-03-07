3ba42c21af2bfdb85dc9183fe6660618689271b4
Allah dad <allahdad.bscsf22@iba-suk.edu.pk>
2025-03-07 12:50:46 +0500

    learning project

---
+++ first_project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>code by Allahdad</h1>
+      <h1>react</h1>
+    </>
+  );
+}
+
+export default App;
